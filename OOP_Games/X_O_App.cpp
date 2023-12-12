2cfe6893cf3f39caacf4307aaa0467acb1312cef
Omer Yassir <omerym10@gmail.com>
2023-12-12 21:14:47 +0200

    Add project files.

---
+++ OOP_Games/X_O_App.cpp
@@ -0,0 +1,41 @@
+// Class definition for XO_App class
+// Author:  Mohammad El-Ramly
+// Date:    10/10/2022
+// Version: 1
+
+#include <iostream>
+#include"BoardGame_Classes.hpp"
+using namespace std;
+
+void game1() {
+    int choice;
+    Player* players[2];
+    players[0] = new Player (1, 'x');
+
+    cout << "Welcome to FCAI X-O Game. :)\n";
+    cout << "Press 1 if you want to play with computer: ";
+    cin >> choice;
+    if (choice != 1)
+        players[1] = new Player (2, 'o');
+    else
+        //Player pointer points to child
+        players[1] = new RandomPlayer ('o', 3);
+
+    GameManager x_o_game (new X_O_Board(), players);
+    x_o_game.run();
+    system ("pause");
+    
+}
+int main()
+{
+    int g;
+    cout << "What game to play?\n";
+    cout << "1. Tic Tac Toe\n";
+    cin >> g;
+    switch (g) 
+    {
+    case 1:
+        game1();
+        break;
+    }
+}
