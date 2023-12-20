5f1a75e730e9e9ecede22d939175fa519121a34b
Omar-Salah123 <sd.omar04@gmail.com>
2023-12-20 18:16:14 +0200

    removed al ramli comment

--- OOP_Games/PyramidXO__20220703.cpp
+++ OOP_Games/PyramidXO__20220703.cpp
@@ -272,25 +272,3 @@ using namespace std;
         delete[] this->board;
     }
     char PyramidXO :: sym=0;
-// class Board {
-// protected:
-//    int n_rows, n_cols;
-//    char** board;
-//    int  n_moves = 0;
-
-// public:
-//    // Return true  if move is valid and put it on board
-//    // within board boundaries in empty cell
-//    // Return false otherwise
-//    virtual bool update_board (int x, int y, char symbol) = 0;
-//    // Returns true if there is any winner
-//    // either X or O
-//    // Written in a complex way. DO NOT DO LIKE THIS.
-//    virtual bool is_winner() = 0;
-//    // Return true if all moves are done and no winner
-//    virtual bool is_draw() = 0;
-//    // Display the board and the pieces on it
-//    virtual void display_board() = 0;
-//    // Return true if game is over
-//    virtual bool game_is_over() = 0;
-// };
